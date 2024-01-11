d7af555ff33fc1543f2876c281ed9b7ff2a4eeab
GAGAN RAGHAV <120806356+GAGANRAGHAV@users.noreply.github.com>
2024-01-11 23:12:15 +0530

    completed chat app

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { db, auth } from "../firebase-config";
 import {
   collection,
@@ -10,17 +10,34 @@ import {
   orderBy,
 } from "firebase/firestore";
 
-const Chat = ({room}) => {
+// import "../styles/Chat.css";
 
-  // const room =props;
+const Chat = ({ room }) => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const messagesRef = collection(db, "messages");
 
-  const [newMessage , setNewMessage] = useState("")
-  const messagesRef = collection(db, "users");
+  useEffect(() => {
+    const queryMessages = query(
+      messagesRef,
+      where("room", "==", room),
+      orderBy("createdAt")
+    );
+    const unsuscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+      setMessages(messages);
+    });
 
+    return () => unsuscribe();
+  }, []);
 
-  const handesubmit = async(e) => {
-    e.preventDefault();
-    console.log(newMessage)
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
     if (newMessage === "") return;
     await addDoc(messagesRef, {
       text: newMessage,
@@ -30,38 +47,34 @@ const Chat = ({room}) => {
     });
 
     setNewMessage("");
-
-
   };
 
-  // const handlesubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   if (newMessage === "") return;
-  //   await addDoc(messagesRef, {
-  //     text: newMessage,
-  //     createdAt: serverTimestamp(),
-  //     user: auth.currentUser.displayName,
-  //     room,
-  //   });
-
-  //   setNewMessage("");
-  // };
-
-
   return (
-    <div>
-        <form onSubmit={handesubmit}>
-          <input placeholder="type msg.."
-
-          onChange={(e) => setNewMessage(e.target.value)}
-
-          
-          ></input>
-          <button type="submit" >send</button>
-        </form>
+    <div className="chat-app">
+      <div className="header">
+        <h1>Welcome to: {room.toUpperCase()}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div key={message.id} className="message">
+            <span className="user">{message.user}:</span> {message.text}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(event) => setNewMessage(event.target.value)}
+          className="new-message-input"
+          placeholder="Type your message here..."
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Chat
